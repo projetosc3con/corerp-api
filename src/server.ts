5b20b44c6ea18cc0ffb7653447384221b9cfc7cf
projetosc3con <projetosc3con@gmail.com>
2025-12-07 15:45:12 -0400

    ajustes roles

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
 import express from 'express';
+import serverless from 'serverless-http';
+import dotenv from 'dotenv';
 import cors from 'cors';
-import usersRoutes from './routes/users';
+
 import rolesRoutes from './routes/roles';
+import usersRoutes from './routes/users'; // caso exista
+
+dotenv.config();
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-// Suas rotas
-app.use('/users', usersRoutes);
 app.use('/roles', rolesRoutes);
+app.use('/users', usersRoutes); // se tiver
 
-export default app;
+export const handler = serverless(app); // <- importante
